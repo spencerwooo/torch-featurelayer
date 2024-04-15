4bfdda76072c2fdc9d60d6473db008550752de04
spencerwooo <spencer.woo@outlook.com>
2024-04-15 14:30:59 +0800

    Use CPU only is ok

--- examples/extract_multiple_layers.py
+++ examples/extract_multiple_layers.py
@@ -3,17 +3,15 @@ from torchvision.models import resnet50
 
 from torch_featurelayer import FeatureLayers
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 # Load a pretrained ResNet-50 model
-model = resnet50(weights='DEFAULT').eval().to(device)
+model = resnet50(weights='DEFAULT').eval()
 
 # Hook onto layers ['layer1', 'layer2', 'layer3', 'layer4'] of the model
 layer_paths = ['layer1', 'layer2', 'layer3', 'layer4']
 hooked_model = FeatureLayers(model, layer_paths)
 
 # Forward pass an input tensor through the model
-x = torch.randn(1, 3, 224, 224).to(device)
+x = torch.randn(1, 3, 224, 224)
 feature_outputs, output = hooked_model(x)
 
 # Print the output shapes
--- examples/extract_one_layer.py
+++ examples/extract_one_layer.py
@@ -3,17 +3,15 @@ from torchvision.models import vgg11
 
 from torch_featurelayer import FeatureLayer
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 # Load a pretrained VGG-11 model
-model = vgg11(weights='DEFAULT').eval().to(device)
+model = vgg11(weights='DEFAULT').eval()
 
 # Hook onto layer `features.15` of the model
 layer_path = 'features.15'
 hooked_model = FeatureLayer(model, layer_path)
 
 # Forward pass an input tensor through the model
-x = torch.randn(1, 3, 224, 224).to(device)
+x = torch.randn(1, 3, 224, 224)
 feature_output, output = hooked_model(x)
 
 # Print the output shape
