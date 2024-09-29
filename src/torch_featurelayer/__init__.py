2b4373961b8d66acf8f9ce4922848f5eb513aab2
spencerwooo <spencer.wushangbo@gmail.com>
2024-09-29 15:20:47 +0800

    Relax dep version constraint

--- src/torch_featurelayer/__init__.py
+++ src/torch_featurelayer/__init__.py
@@ -2,5 +2,5 @@ from torch_featurelayer.feature_layer import FeatureLayer
 from torch_featurelayer.feature_layers import FeatureLayers
 from torch_featurelayer.layer_candidates import get_layer_candidates
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __all__ = ['FeatureLayer', 'FeatureLayers', 'get_layer_candidates']
